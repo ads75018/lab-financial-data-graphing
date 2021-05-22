6cb8cdab675cb24ce80ab2663170750e53e4f063
Adam Ghaoul <adamghaoul@gmail.com>
2021-05-22 14:04:45 +0200

    finished sorta

---
+++ public/javascripts/financial-data.js
@@ -0,0 +1,40 @@
+const input = document.querySelector("input");
+const startDate = document.getElementById("start-date");
+const endDate = document.getElementById("end-date");
+
+axios
+  .get("http://api.coindesk.com/v1/bpi/historical/close.json")
+  .then(function (response) {
+    printTheChart(response.data);
+  })
+  .catch((err) => console.log(err));
+
+function printTheChart(bigData) {
+  const bpi = bigData["bpi"];
+  const dates = Object.keys(bpi);
+  const moolah = Object.values(bpi);
+  console.log(moolah);
+  console.log(startDate)
+
+  // on essaie de faire une fonction, à voir.. 
+  if(startDate.value === "" && endDate.value === ""){
+    
+}
+
+
+  const ctx = document.getElementById("my-chart").getContext("2d");
+  const chart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: dates,
+      datasets: [
+        {
+          label: "Bitcoin Stock Chart",
+          backgroundColor: "rgb(0,255,255)",
+          borderColor: "rgba(46, 49, 49, 1)",
+          data: moolah,
+        },
+      ],
+    },
+  });
+}

89530cba0817f4ec3a4456e1f0e2909118f8b4ce
Adam Ghaoul <adamghaoul@gmail.com>
2021-05-22 14:54:32 +0200

    finished

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -2,6 +2,51 @@ const input = document.querySelector("input");
 const startDate = document.getElementById("start-date");
 const endDate = document.getElementById("end-date");
 
+
+
+
+
+
+input.onchange = function () {
+  const newStartDate = startDate.value;
+  const newEndDate = endDate.value
+ 
+  
+
+  axios
+    .get(
+      `http://api.coindesk.com/v1/bpi/historical/close.json?start=${newStartDate}&end=${newEndDate}`
+    )
+    .then(function (response) {
+      printTheChart(response.data);
+    })
+    .catch((err) => console.log(err));
+
+  function printTheChart(bigData) {
+    const bpi = bigData["bpi"];
+    const dates = Object.keys(bpi);
+    const moolah = Object.values(bpi);
+    console.log(moolah);
+    console.log(startDate);
+
+    const ctx = document.getElementById("my-chart").getContext("2d");
+    const chart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: dates,
+        datasets: [
+          {
+            label: "Bitcoin Stock Chart",
+            backgroundColor: "rgb(0,255,255)",
+            borderColor: "rgba(46, 49, 49, 1)",
+            data: moolah,
+          },
+        ],
+      },
+    });
+  }
+};
+
 axios
   .get("http://api.coindesk.com/v1/bpi/historical/close.json")
   .then(function (response) {
@@ -15,12 +60,6 @@ function printTheChart(bigData) {
   const moolah = Object.values(bpi);
   console.log(moolah);
   console.log(startDate)
-
-  // on essaie de faire une fonction, à voir.. 
-  if(startDate.value === "" && endDate.value === ""){
-    
-}
-
 
   const ctx = document.getElementById("my-chart").getContext("2d");
   const chart = new Chart(ctx, {
